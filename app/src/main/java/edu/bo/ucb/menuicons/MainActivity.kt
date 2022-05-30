06c4046edd371d345f6dd84c4ab7b6df09593ffe
MercedesLaFuente <65575674+MercedesLaFuente@users.noreply.github.com>
2022-05-30 12:19:25 -0400

    Initial commit

---
+++ app/src/main/java/edu/bo/ucb/menuicons/MainActivity.kt
@@ -0,0 +1,33 @@
+package edu.bo.ucb.menuicons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+
+            R.id.action_search ->{
+                Log.d("TEST", "ICON SEARCH")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
